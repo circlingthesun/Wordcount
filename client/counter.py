14d0a8385efbef9c552768084b89a94bf4920bea
Rickert Mulder <circlingthesun@gmail.com>
2011-10-23 04:59:09 +0200

    Added docx support

--- client/counter.py
+++ client/counter.py
@@ -2,26 +2,46 @@
 import sys, subprocess, os, time
 import urllib
 import ConfigParser
+from docxcount import countdocx
 
 config = ConfigParser.ConfigParser()
 sfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "settings.ini")
 config.read(sfile)
 
+# for non docx
 interpreter = config.get('client', 'script_interpreter')
 count_script = config.get('client', 'count_script')
 count_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), count_script)
+
+# for docx
+docx_file = None
+
+try:
+    docx_file = config.get('client', 'docx_file')
+    docx_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), docx_file)
+except:
+    pass
+
+# settings
 name = config.get('client', 'name')
 url = config.get('client', 'update_url')
 interval = int(config.get('client', 'update_interval'))
 
 def run():
     while True:
-        args = [interpreter, count_script]
-        count,error = subprocess.Popen(args,stdout = subprocess.PIPE,
-                stderr= subprocess.PIPE).communicate()
-        if error:
-            print "Error %s" % error
-        params = urllib.urlencode({'c': count, 'name': name})
+    
+        count = 0
+        
+        if docx_file :
+            count = countdocx(docx_file)
+        else:
+            args = [interpreter, count_script]
+            count,error = subprocess.Popen(args,stdout = subprocess.PIPE,
+                    stderr= subprocess.PIPE).communicate()
+            if error:
+                print "Error %s" % error
+        
+        params = urllib.urlencode({'c': str(count), 'name': name})
         
         try:
             f = urllib.urlopen(url, params)
---
+++ client/docxcount.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+import sys, shutil, os
+import zipfile
+from xml.sax.handler import ContentHandler
+from xml import sax
+
+def getXml(filename):
+    with zipfile.ZipFile(filename) as myzip:
+        fname = myzip.extract('word/document.xml', '.')
+        val =  open(fname).read()
+        shutil.rmtree(os.path.dirname(fname))
+        return val
+
+class textHandler(ContentHandler):
+    text = []
+    def characters(self, ch):
+        self.text.append(ch.encode("Latin-1"))
+
+def gettext(xmldata):
+    handler = textHandler()
+    sax.parseString(xmldata, handler)
+    return " ".join(handler.text)
+        
+def wordcount(text):
+    # assumes words are separated by whitespace
+    print text.split(None)
+    return len(text.split(None))
+
+def countdocx(filename):
+    xml = getXml(filename)
+    text = gettext(xml)
+    return wordcount(text)
+
+if __name__ == '__main__':        
+    try:
+        print countdocx(sys.argv[1])  
+    except:    
+        exit()   
