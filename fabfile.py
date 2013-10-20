762bfec25487760028a71d3900f2b2b990de5759
Rickert Mulder <circlingthesun@gmail.com>
2013-10-21 00:39:43 +0200

    Tweaked fab file

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,13 @@
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-env.hosts = ['rickert.co.za']
+env.hosts = ['words.jacklab.co.za']
 env.user = 'rickert'
 #env.password = ''
 env.remote_dev = "rickert.co.za"
-env.remote_dir = "/home/rickert/webapps/Wordcount.git"
-
-code_dir = '/home/rickert/webapps/Wordcount'
+env.remote_dir = "/home/rickert/Wordcount.git"
+code_dir = '/home/rickert/Wordcount'
+repo = "ssh://git@github.com:circlingthesun/Wordcount.git"
 
 def zipclient():
     local('zip -r wserver/static/client.zip wclient -x \*.pyc \*~ \*.ui')
@@ -17,16 +17,16 @@ def commit():
     local("git add -p && git commit")
 
 def push_to_remote():
-    local("git push ssh://%(user)s@%(remote_dev)s/%(remote_dir)s master" % env)
+    local("git push %s" % repo)
+    #local("git push ssh://%(user)s@%(remote_dev)s/%(remote_dir)s master" % env)
 
 def pull_remote():
     with cd(code_dir):
         #run("git reset --hard HEAD")
         #run("git checkout master")
-        run("git pull %s" % env.remote_dir)
+        run("git pull %s" % repo)
         #run("git reset --hard HEAD")
         run("source env/bin/activate && python setup.py develop")
-        run("touch wserver/count.wsgi")
 
 def deploy():
     commit()
@@ -34,4 +34,4 @@ def deploy():
     pull_remote()
     
 def update():
-    local("git pull ssh://%(user)s@%(remote_dev)s/%(remote_dir)s" % env)
+    local("git pull %s" % repo)
